5549db1fbdc031471b1ab1bcd8b98292bb76d4f6
Șuchea Rareș-Andrei <rasuchea75@gmail.com>
2024-04-19 00:56:01 +0300

    further modifications to domain classes

--- Domain/Question.cs
+++ Domain/Question.cs
@@ -1,6 +1,6 @@
 ﻿namespace UBB_SE_2024_Team_42.Domain
 {
-    public class Question(long newPostID, long newUserID, string newTitle, string newCategory, string newContent, DateTime newDatePosted, DateTime newDateOfLastEdit, bool newEditFlag, string newPostType, List<Vote> newVoteList, List<Tag> newTagList) : Post(newPostID, newUserID, newContent, newPostType, newVoteList, newDatePosted, newDateOfLastEdit)
+    public class Question(long newPostID, long newUserID, string newTitle, string newCategory, string newContent, DateTime newDatePosted, DateTime newDateOfLastEdit, string newPostType, List<Vote> newVoteList, List<Tag> newTagList) : Post(newPostID, newUserID, newContent, newPostType, newVoteList, newDatePosted, newDateOfLastEdit)
     {
         public string Title { get; set; } = newTitle;
         public string Category { get; } = newCategory;
