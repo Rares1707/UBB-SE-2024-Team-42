ea1555c010d0e476e60a8d5ae2332d5756c8ec65
Șuchea Rareș-Andrei <rasuchea75@gmail.com>
2024-04-19 13:58:21 +0300

    repository.getAllUsers() is done

--- Repository/Repository.cs
+++ Repository/Repository.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using UBB_SE_2024_Team_42.Domain;
+using System.Drawing;
+using System.IO;
 
 namespace UBB_SE_2024_Team_42.Repository
 {
@@ -21,20 +24,108 @@ namespace UBB_SE_2024_Team_42.Repository
             this.sqlConnectionString = sqlConnectionString;
         }
 
-        public void getAllUsers()
+        public List<Notification> getNotificationsOfUser(long userId)
         {
             SqlConnection connection = new SqlConnection(sqlConnectionString);
             connection.Open();
-
-            SqlCommand command = new SqlCommand("select * from dbo.getAllUsers()\r\n", connection);
-            //command.CommandType = System.Data.CommandType.Func
+            SqlCommand command = new SqlCommand("select * from dbo.getNotificationsOfUser(" + userId + ")", connection);
             SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
-            DataTable data = new DataTable();
-            dataAdapter.Fill(data);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
 
-            //now make a list of Users and return it
-            //string str = data.Rows[0][0].ToString();
-            //Console.WriteLine(str);
+            List<Notification> notificationList = new List<Notification>();
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                notificationList.Add(new Notification(
+                    Convert.ToInt64(dataTable.Rows[i]["id"]), 
+                    "Placeholder text", //this should be created in the constructor without needing this parameter
+                    Convert.ToInt64(dataTable.Rows[i]["postId"]), 
+                    Convert.ToInt64(dataTable.Rows[i]["badgeId"])
+                    ));
+            }
+            connection.Close();
+
+            return notificationList;
+        }
+
+        public List<Category> getCategoriesModeratedByUser(long userId)
+        {
+            SqlConnection connection = new SqlConnection(sqlConnectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand("select * from dbo.getCategoriesModeratedByUser(" + userId + ")", connection);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+
+            List<Category> categoryList = new List<Category>();
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                categoryList.Add(new Category(
+                    Convert.ToInt64(dataTable.Rows[i]["id"]),
+                    dataTable.Rows[i]["name"].ToString()
+                    ));
+            }
+            connection.Close();
+
+            return categoryList;
+        }
+
+        public List<Badge> getBadgesOfUser(long userId)
+        {
+            SqlConnection connection = new SqlConnection(sqlConnectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand("select * from dbo.getBadgesOfUser(" + userId + ")", connection);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+
+            List<Badge> badgeList = new List<Badge>();
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                Image badgeImage;
+                byte[] imageBytes = (byte[])dataTable.Rows[i]["image"];
+                using (MemoryStream stream = new MemoryStream(imageBytes))
+                { 
+                    badgeImage = Image.FromStream(stream);
+                }
+                badgeList.Add(new Badge(
+                    Convert.ToInt64(dataTable.Rows[i]["id"]),
+                    dataTable.Rows[i]["name"].ToString(),
+                    dataTable.Rows[i]["description"].ToString(),
+                    badgeImage
+                    ));
+            }
+            connection.Close();
+
+            return badgeList;
+        }
+
+
+
+        public List<User> getAllUsers()
+        {
+            SqlConnection connection = new SqlConnection(sqlConnectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand("select * from dbo.getCategoriesModeratedByUser(" + userId + ")", connection);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+
+            List<User> userList = new List<User>();
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                long userId = Convert.ToInt64(dataTable.Rows[i]["id"]);
+                userList.Add(new User(
+                    userId,
+                    dataTable.Rows[i]["name"].ToString(),
+                    getNotificationsOfUser(userId),
+                    getCategoriesModeratedByUser(userId),
+                    getBadgesOfUser(userId)
+                    ));
+            }
+            connection.Close();
+
+            return userList;
         }
         
     }
