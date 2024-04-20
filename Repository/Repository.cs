655759b7ace4f0b1a3c7b1d3750ccc4565cbef5d
Șuchea Rareș-Andrei <rasuchea75@gmail.com>
2024-04-20 17:30:27 +0300

    connected Posts functions of DB to repo

--- Repository/Repository.cs
+++ Repository/Repository.cs
@@ -196,7 +196,128 @@ namespace UBB_SE_2024_Team_42.Repository
 
         public void addQuestion(Question question)
         {
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionString);   
+            sqlConnection.Open();
 
+            //TODO finish this
+        }
+
+        public List<Tag> getTagsOfQuestion(long questionId)
+        {
+            SqlConnection connection = new SqlConnection(sqlConnectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand("select * from dbo.getTagsOfQuestions(" + questionId + ")", connection);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+
+            List<Tag> tagList = new List<Tag>();
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                tagList.Add(new Tag(Convert.ToInt64(dataTable.Rows[i]["id"]), dataTable.Rows[i]["name"].ToString()));
+            }
+            connection.Close();
+
+            return tagList;
+        }
+
+        // Questions DB Functions
+        public Question getQuestion(long questionId)
+        {
+            SqlConnection connection = new SqlConnection(sqlConnectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand("select * from dbo.getQuestionByID(" + questionId + ")", connection);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+
+            DataTable dataTable = new DataTable();
+
+            dataAdapter.Fill(dataTable);
+
+            List<Tag> tagList = getTagsOfQuestion(questionId);
+            List<Vote> voteList = getVotesOfPost(questionId);
+            Category category = getCategory(Convert.ToInt64(dataTable.Rows[0]["categoryId"]));
+
+            connection.Close();
+
+            return new Question(Convert.ToInt64(dataTable.Rows[0]["id"]), Convert.ToInt64(dataTable.Rows[0]["userId"]),
+                                dataTable.Rows[0]["title"].ToString(), category,
+                                dataTable.Rows[0]["content"].ToString(), Convert.ToDateTime(dataTable.Rows[0]["datePosted"]),
+                                Convert.ToDateTime(dataTable.Rows[0]["dateOfLastEdit"]), dataTable.Rows[0]["type"].ToString(),
+                                voteList, tagList);
+        }
+
+        public List<Question> getAllQuestions()
+        {
+            SqlConnection connection = new SqlConnection(sqlConnectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand("select * from dbo.getAllQuestions", connection);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+
+            List<Question> questionList = new List<Question>();
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                questionList.Add(getQuestion(Convert.ToInt64(dataTable.Rows[i]["id"])));
+            }
+            connection.Close();
+
+            return questionList;
+        }
+
+        public Category getCategory(long categoryId)
+        {
+            SqlConnection connection = new SqlConnection(sqlConnectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand("select * from dbo.getCategoryByID(" + categoryId + ")", connection);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+
+            DataTable dataTable = new DataTable();
+
+            dataAdapter.Fill(dataTable);
+
+            connection.Close();
+
+            return (new Category(Convert.ToInt64(dataTable.Rows[0]["id"]), dataTable.Rows[0]["name"].ToString()));
+        }
+
+        public List<Post> getRepliesOfPost(long postId)
+        {
+            SqlConnection connection = new SqlConnection(sqlConnectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand("select * from dbo.GetAllRepliesOfPost(" + postId + ")", connection);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+
+            List<Post> postList = new List<Post>();
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                Post newPost;
+                long id = Convert.ToInt64(dataTable.Rows[i]["id"]);
+                long userId = Convert.ToInt64(dataTable.Rows[i]["userId"]);
+                DateTime datePosted = Convert.ToDateTime(dataTable.Rows[i]["datePosted"]);
+                DateTime dateOfLastEdit = Convert.ToDateTime(dataTable.Rows[i]["dateOfLastEdit"];
+                string type = dataTable.Rows[i]["type"].ToString();
+                string content = dataTable.Rows[i]["content"].ToString();
+                List<Vote> votes = getVotesOfPost(postId);
+                if (type == Post.QUESTION_TYPE)
+                {
+                    string title = dataTable.Rows[i]["title"].ToString();
+                    Category category = getCategory(Convert.ToInt64(dataTable.Rows[i]["categoryId"]));
+                    List<Tag> tags = getTagsOfQuestion(postId);
+                    newPost = new Question(postId, userId, title, category, content, datePosted, dateOfLastEdit, type, votes, tags);
+                }
+                else
+                {
+                    newPost = new Post(postId, userId, content, type, votes, datePosted, dateOfLastEdit);
+                }
+
+                postList.Add(newPost);
+            }
+
+            connection.Close();
+            return postList;
         }
 
     }
