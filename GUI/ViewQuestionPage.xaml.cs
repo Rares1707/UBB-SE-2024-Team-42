a528a3132374f7d7fb91c635c0f2794724e29bb8
Darius Timoce <74290441+MadeOfDye@users.noreply.github.com>
2024-04-20 17:37:09 +0300

    Comment Created and View Question Page Created also

--- GUI/ViewQuestionPage.xaml.cs
+++ GUI/ViewQuestionPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,17 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace UBB_SE_2024_Team_42
+namespace UBB_SE_2024_Team_42.GUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ViewQuestionPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ViewQuestionPage : Page
     {
-        public MainWindow()
+        public ViewQuestionPage()
         {
             InitializeComponent();
-            MainFrame.Navigate(new Uri("GUI/SearchQuestionPage.xaml", UriKind.Relative));
         }
     }
 }
